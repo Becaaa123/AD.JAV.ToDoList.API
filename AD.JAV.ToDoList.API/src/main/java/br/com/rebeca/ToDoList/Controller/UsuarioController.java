3f235b56a1e98b11e6ccffe68373b38fa6d983ae
Rebeca <rebeca.mauricio@araujo.com.br>
2025-06-18 14:08:30 -0300

    alterações no endpoint de cadastro de usuario

--- AD.JAV.ToDoList.API/src/main/java/br/com/rebeca/ToDoList/Controller/UsuarioController.java
+++ AD.JAV.ToDoList.API/src/main/java/br/com/rebeca/ToDoList/Controller/UsuarioController.java
@@ -21,10 +21,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Log
@@ -88,26 +90,49 @@ public class UsuarioController extends BaseController {
             @ApiResponse(responseCode = "504", description = "Gateway Timeout - Tempo de resposta excedido")
     })
     @PostMapping("/cadastro")
-    public ResponseEntity<BaseResponseDTO> cadastrarUsuario(
-            @Parameter(description = "Cadastrar novo usuario no sistema")
-            @RequestBody(required = true) UsuarioDTO usuario,
-            Authentication authentication) {
-        try {
-            Jwt jwt = (Jwt) authentication.getPrincipal();
-            String tokenEmail = jwt.getClaim("email");
+    public ResponseEntity cadastarUsuario(
+            @RequestParam )
 
-            return response(HttpStatus.OK, usuarioService.cadastraUsuario(usuario, tokenEmail), "Usuario cadastrado com sucesso" ,SUCCESS);
-        } catch (BaseException be) {
-            log.warning(be.getMessage());
-
-            return errorWithStatusCode(be.getMessage(), be.getHttpStatus());
-        } catch (Exception exception) {
-            log.warning(exception.getMessage() + exception);
-
-            return errorWithStatusCode(OCORREU_UM_ERRO_DESCONHECIDO_CONTATE_O_ADMINISTRADOR_DO_SISTEMA, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
+//    @RequestParam(required = false) Integer idPedidoHistorico,
+//    @RequestParam(required = false) String idPedidoMongo) {
+//        BaseResponseDTO baseResponseDTO = new BaseResponseDTO();
+//    public  ResponseEntity consultarPedido(@PathVariable Integer numeroPedido){
+//        BaseResponseDTO baseResponseDTO = new BaseResponseDTO();
+//        try {
+//            var dadosConsulta = pedidoService.consultarPedido(numeroPedido);
+//            return ResponseEntity.ok(BaseResponseDTO.builder()
+//                    .data(dadosConsulta)
+//                    .build()
+//            );
+//        }catch (BusinessException be) {
+//            baseResponseDTO.setMessage(be.getMessage());
+//            return ResponseEntity.status(be.getHttpStatus()).body(baseResponseDTO);
+//        } catch (Exception e){
+//            baseResponseDTO.setMessage("Erro interno ao consultar o pedido.");
+//            log.error("Erro interno ao consultar número do pedido {}: {}", numeroPedido, e.getMessage());
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(baseResponseDTO);
+//        }
+//    }
+//    @PostMapping("/cadastro")
+//    public ResponseEntity<BaseResponseDTO> cadastrarUsuario(
+//            @Parameter(description = "Cadastrar novo usuario no sistema")
+//            @RequestBody(required = true) UsuarioDTO usuario,
+//            Authentication authentication) {
+//        try {
+//            Jwt jwt = (Jwt) authentication.getPrincipal();
+//            String tokenEmail = jwt.getClaim("email");
+//
+//            return response(HttpStatus.OK, usuarioService.cadastraUsuario(usuario, tokenEmail), "Usuario cadastrado com sucesso" ,SUCCESS);
+//        } catch (BaseException be) {
+//            log.warning(be.getMessage());
+//
+//            return errorWithStatusCode(be.getMessage(), be.getHttpStatus());
+//        } catch (Exception exception) {
+//            log.warning(exception.getMessage() + exception);
+//
+//            return errorWithStatusCode(OCORREU_UM_ERRO_DESCONHECIDO_CONTATE_O_ADMINISTRADOR_DO_SISTEMA, HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
     @Operation(summary = "Realiza a atualização de usuarios")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Success", content = {
